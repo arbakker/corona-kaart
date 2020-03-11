304bbea3ff3b4773da0007e791683b43af504599
Anton Bakker <anton.bakker@kadaster.nl>
2020-03-11 15:23:41 +0100

    fixed legend

--- scripts/process-data.sh
+++ scripts/process-data.sh
@@ -1,41 +1,52 @@
 #!/usr/bin/env bash
+
 set -eu
 
 BASE_URL="https://www.volksgezondheidenzorg.info/sites/default/files/map/detail_data/klik_corona"
-DEST_GPKG="data/corona_nl.gpkg"
-DEST_CSV="data/corona.csv"
-DEST_CSV_FIXED="data/corona_fix_gem_codes.csv"
-DEST_CSVT_FIXED="data/corona_fix_gem_codes.csvt"
+DEST_GPKG="../data/corona_nl.gpkg"
+DEST_CSV="../data/corona.csv"
+DEST_CSV_FIXED="../data/corona_fix_gem_codes.csv"
+DEST_CSVT_FIXED="../data/corona_fix_gem_codes.csvt"
 
-function get_url(){
-    current_date=$(date +%d%m%Y)
-    url="${BASE_URL}${current_date}.csv"
-    status_code=$(curl -s -I -w "%{http_code}" "$url" -o /dev/null | tr '\r' '\n')
-    if [[ $status_code == 200 ]];then
-        echo "$url"
-        return 0
-    fi
-    for x in $(seq 0 19); do
-        echo "$x"
-        url="${BASE_URL}${current_date}_${x}.csv"
-        status_code=$(curl -s -I -w "%{http_code}" "$url" -o /dev/null | tr '\r' '\n')
-        if [[ "$status_code" -eq 200 ]];then
-            echo "$url"
-            return 0
-        fi
-    done 
-    echo ""
-    return 1
+DATE_DATA=""
+URL_DATA=""
+
+
+function get_url() {
+    # echo "https://www.volksgezondheidenzorg.info/sites/default/files/map/detail_data/klik_corona$(date +%d%m%Y)_2.csv"
+    echo https://www.volksgezondheidenzorg.info/sites/default/files/map/detail_data/klik_corona10032020_2.csv
+    # current_date=$(date +%d%m%Y)
+    # url="${BASE_URL}${current_date}.csv"
+    # status_code=$(curl -s -I -w "%{http_code}" "$url" -o /dev/null | tr '\r' '\n')
+    # echo "$url"
+    # if [[ $status_code == 200 ]];then
+    #     echo "$url"
+    #     return 0
+    # fi
+    # for x in $(seq 0 19); do
+    #     echo "$x"
+    #     url="${BASE_URL}${current_date}_${x}.csv"
+    #     echo "$url"
+    #     status_code=$(curl -s -I -w "%{http_code}" "$url" -o /dev/null | tr '\r' '\n')
+    #     if [[ "$status_code" -eq 200 ]];then
+    #         echo "$url"
+    #         return 0
+    #     fi
+    # done 
+    # echo ""
+    # return 1
 }
 
 function download() {
     csv_url=$(get_url)
+    echo "$csv_url"
     if [[ -z $csv_url ]];then
         echo "could not determine CSV url"
-        exit 1
+        return 1
     fi
-    echo "{\"date_data\": \"$(date +%d-%m-%Y)\",\"url\": \"$csv_url\"}" > webapp/updated.json
-    curl "$csv_url" -o "$DEST_CSV"
+    DATE_DATA=$(date +%d-%m-%Y)
+    URL_DATA=$csv_url
+    curl -v "$csv_url" -o "$DEST_CSV"
     # mkdir -p data/src
     # curl https://geodata.nationaalgeoregister.nl/bestuurlijkegrenzen/extract/bestuurlijkegrenzen.zip -o data/src/bestuurlijkegrenzen.zip
     # unzip data/src/bestuurlijkegrenzen.zip -d data/src/
@@ -50,9 +61,10 @@ function fix_gem_codes(){
     while read -r line; do
         code=$(echo $line | awk -F';' '{printf($1)}' | xargs -0 printf "%04d\n")
         echo "$code;$(echo $line | awk -F';' '{printf($2)}');$(echo $line | awk -F';' '{printf($3)}');$(echo $line | awk -F';' '{printf($4)}')" >> "$dest_file"
-    done< <(tail -n +2 data/corona.csv)
+    done< <(tail -n +2 "$input_file")
 }
 
+echo "TEST"
 # call functions
 download
 fix_gem_codes
@@ -65,6 +77,8 @@ ogr2ogr -append -f GPKG "$DEST_GPKG" "$DEST_CSV_FIXED" corona_fix_gem_codes -nln
 ogrinfo "$DEST_GPKG" -sql "update corona_stats set Aantal=0 where Aantal IS NULL"
 ogr2ogr -append -f GPKG "$DEST_GPKG" "$DEST_GPKG" -sql "select gemeenten.*, corona_stats.Aantal as aantal from gemeenten left join corona_stats on gemeenten.Code = corona_stats.id" -nln gemeenten_corona
 
+TOTAL=$(ogrinfo data/corona_nl.gpkg -sql "select sum(aantal) as total from gemeenten_corona"  | grep "total " | cut -d= -f 2)
+echo "{\"date_data\": \"$DATE_DATA\",\"url\": \"$URL_DATA\", \"total_infections\":\"$TOTAL\"}" > webapp/updated.json
 
 # duplicated features (foreach case 1 feature is required)
 # quite inefficient...
@@ -88,8 +102,10 @@ geo2topo -q 1e5 -n gemeenten_borders_simplified=data/gemeenten_ndjson.json | top
 geo2topo -q 1e5 -n gemeenten_borders_outside=data/gemeenten_ndjson.json | toposimplify -s 0.00000001 -f | topomerge --mesh -f 'a === b' gemeenten_borders_outside=gemeenten_borders_outside  | topo2geo data/gemeenten_borders_outside.json 
 
 # copy assed to webapp/data
-mkdir -p webapp/data
-cp "data/corona_markers.json" webapp/data/
-cp data/gemeenten_simplified.json webapp/data/
-cp data/gemeenten_borders_simplified.json webapp/data/
-cp data/gemeenten_borders_outside.json webapp/data/
+mkdir -p ../webapp/data
+cp ../data/corona_markers.json ../webapp/data/
+cp ../data/gemeenten_simplified.json ../webapp/data/
+cp ../data/gemeenten_borders_simplified.json ../webapp/data/
+cp ../data/gemeenten_borders_outside.json ../webapp/data/
+
+python classify.py ../data/corona_nl.gpkg 5 --layer gemeenten_corona --attribute aantal > ../webapp/classes.json
--- webapp/index.js
+++ webapp/index.js
@@ -9,21 +9,22 @@ import './node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css'
 import './index.css'
 import coronaMarkers from './data/corona_markers.json'
 import gemeenten from './data/gemeenten_simplified.json'
+import gemeentenPoint from './data/gemeenten_simplified_point.json'
 import gemeentenBorders from './data/gemeenten_borders_simplified.json'
 import gemeentenBordersOutside from './data/gemeenten_borders_outside.json'
 import updated from './updated.json'
+import classes from './classes.json'
 
 /* global L */
-
 var map = L.map('mapid', {
   crs: L.CRS.EPSG3857,
   maxZoom: 18,
   minZoom: 5,
-  maxBounds: [[43.934028,-4.262695],
+  maxBounds: [[43.934028, -4.262695],
     [58.378797, 13.886719]]
 }).setView([52.505, 5], 8)
 
-var backgroundLayer = L.tileLayer('https://geodata.nationaalgeoregister.nl/tiles/service/wmts/brtachtergrondkaart/EPSG:3857/{z}/{x}/{y}.png',
+var backgroundLayer = L.tileLayer('https://geodata.nationaalgeoregister.nl/tiles/service/wmts/brtachtergrondkaartwater/EPSG:3857/{z}/{x}/{y}.png',
   {
     wmts: true,
     attribution: 'BRT-Achtergrondkaart: © <a href="http://www.kadaster.nl">Kadaster</a> (<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>)</span>'
@@ -34,9 +35,9 @@ const gemBordersLayer = L.geoJSON(gemeentenBorders, {
   style: function (feature) {
     return {
       fillColor: null,
-      weight: 0.5,
+      weight: 1,
       opacity: 1,
-      color: '#454f4e',
+      color: '#878787',
       dashArray: '4',
       fillOpacity: 0
     }
@@ -61,9 +62,9 @@ const gemBordersLayerOutside = L.geoJSON(gemeentenBordersOutside, {
   style: function (feature) {
     return {
       fillColor: null,
-      weight: 1,
+      weight: 2,
       opacity: 1,
-      color: 'darkgray',
+      color: '#878787',
       dashArray: '4',
       fillOpacity: 0
     }
@@ -72,15 +73,15 @@ const gemBordersLayerOutside = L.geoJSON(gemeentenBordersOutside, {
 
 // markers layer
 var markers = L.markerClusterGroup({
-  polygonOptions:{
-      fillColor: '#e7e1ef',
-      weight: 1,
-      opacity: 1,
-      color: '#dd1c77',
-      fillOpacity: 0.5
+  polygonOptions: {
+    fillColor: '#e7e1ef',
+    weight: 1,
+    opacity: 1,
+    color: '#dd1c77',
+    fillOpacity: 0.5
   },
   singleMarkerMode: true,
-  attribution: `<a href="${updated.url}">Data coronavirus besmettingen - ${updated.date_data}</a>: <a href="https://www.volksgezondheidenzorg.info/onderwerp/infectieziekten/regionaal-internationaal/coronavirus-covid-19">RIVM</a>`
+  attribution: 'Data coronavirus besmettingen: <a href="https://www.volksgezondheidenzorg.info/onderwerp/infectieziekten/regionaal-internationaal/coronavirus-covid-19">RIVM</a>'
 })
 const geojsonMarkers = L.geoJSON(coronaMarkers)
 markers.addLayers(geojsonMarkers)
@@ -98,4 +99,100 @@ markers.on('click', function (a) {
     .setLatLng([a.layer._latlng.lat, a.layer._latlng.lng])
     .setContent('<p><b>Gemeente: </b>' + a.layer.feature.properties.gemeentenaam + '</p>')
     .openOn(map)
+})
+
+function getColor (value) {
+  const colors = ['#ffffcc', '#a1dab4', '#41b6c4', '#2c7fb8', '#253494']
+  const breaks = classes.gemeenten_corona.aantal
+  let color
+
+  for (let i = 1; i < breaks.length; i++) {
+    if (i === 1) {
+      if (value > breaks[0] && value <= breaks[i]) {
+        color = colors[i - 1]
+        break
+      }
+    } else {
+      if (value <= breaks[i]) {
+        color = colors[i - 1]
+        break
+      }
+    }
+  }
+  return color
+}
+function iconByStations (feature) {
+  if (feature.properties.aantal > 0) {
+    var calculatedSize = (8 + (feature.properties.aantal * 3))
+    var CustomIcon = L.Icon.extend({
+      options: {
+        iconSize: [calculatedSize, calculatedSize],
+        iconAnchor: [calculatedSize / 3, calculatedSize / 2]
+      }
+    })
+    const color = getColor(feature.properties.aantal)
+    const achenSvgString = `<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100'><circle cx='50' cy='50' r='40' stroke='gray' stroke-width='2' fill='${color}' /></svg>`
+    const myIconUrl = encodeURI('data:image/svg+xml,' + achenSvgString).replace('#', '%23')
+    var rectIcon = new CustomIcon({ iconUrl: myIconUrl })
+    return rectIcon
+  }
+}
+
+function getLegend () {
+  if (map.hasLayer(metroLayer)) {
+    return '<div class="legend-div"><div style="background-color:#ffffcc;" class="marker-cluster legend-item"></div>1 Besmettingen</div>' +
+    '<div class="legend-div"><div style="background-color:#a1dab4;" class="marker-cluster legend-item"></div>1-4 Besmettingen</div>' +
+    '<div class="legend-div"><div style="background-color:#41b6c4;" class="marker-cluster legend-item"></div>5-11 Besmettingen</div>' +
+    '<div class="legend-div"><div style="background-color:#2c7fb8;" class="marker-cluster legend-item"></div>12-27 Besmettingen</div>' +
+    '<div class="legend-div"><div style="background-color:#253494;" class="marker-cluster legend-item"></div>28-29 Besmettingen</div>'
+  } else {
+    return '<div class="legend-div"><div class="marker-cluster-small legend-item"></div>0-9 Besmettingen</div>' +
+     '<div class="legend-div"><div class="marker-cluster-medium legend-item"></div>10-99 Besmettingen</div>' +
+     '<div class="legend-div"><div class="marker-cluster-large legend-item"></div>>99 Besmettingen</div>'
+  }
+}
+
+// create the GeoJSON layer and call the styling function with each marker
+var metroLayer = L.geoJSON(gemeentenPoint, {
+  pointToLayer: function (feature, latlng) {
+    if (feature.properties.aantal > 0) {
+      return L.marker(latlng, { icon: iconByStations(feature) })
+    }
+  }
+}).bindPopup(function (layer) {
+  return '<p><b>Gemeente: </b>' + layer.feature.properties.Gemeentenaam + '</p>' +
+    '<p><b>Aantal gevallen: </b>' + layer.feature.properties.aantal + '</p>'
+})
+
+L.Control.Command = L.Control.extend({
+  options: {
+    position: 'topright'
+  },
+
+  onAdd: function (map) {
+    console.log(this)
+    var controlDiv = L.DomUtil.create('div', 'leaflet-control-command')
+    var controlUI = L.DomUtil.create('div', 'leaflet-control-command-interior', controlDiv)
+    controlUI.innerHTML = '<h1 style="margin-bottom:5px;">Corona Besmettingen in Nederland</h1><div id="legend">' + getLegend() +
+     '</div><button id="testButton">Style</button>' +
+     `<p>Totaal aantal besmettingen: ${updated.total_infections}&nbsp;&nbsp;|&nbsp;&nbsp;publicatie datum van de <a href="${updated.url}">data</a>: ${updated.date_data}&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://github.com/arbakker/corona-kaart">broncode</a></p>`
+    return controlDiv
+  }
+})
+
+L.control.command = function (options) {
+  return new L.Control.Command(options)
+}
+
+L.control.command({}).addTo(map)
+
+document.getElementById('testButton').addEventListener('click', function () {
+  if (map.hasLayer(metroLayer)) {
+    map.removeLayer(metroLayer)
+    map.addLayer(markers)
+  } else if (map.hasLayer(markers)) {
+    map.removeLayer(markers)
+    map.addLayer(metroLayer)
+  }
+  document.getElementById('legend').innerHTML = getLegend()
 })
